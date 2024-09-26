28cf436d47e32774837d4ebb2f9eb644ccd1c238
Breea Toomey <breeatoomey@Breeas-MacBook-Pro-2.local>
2024-09-25 19:08:08 -0700

    initialized app

---
+++ induo/induoApp.swift
@@ -0,0 +1,17 @@
+//
+//  induoApp.swift
+//  induo
+//
+//  Created by Breea Toomey on 9/25/24.
+//
+
+import SwiftUI
+
+@main
+struct induoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
